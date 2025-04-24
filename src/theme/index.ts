6612ac7784ee19ca1b0e053e1b0f08340fa9b9eb
Austen Tucker <austen@thearcades.me>
2025-04-24 12:08:33 -0500

    feat(theme): add color mode configuration and integrate ColorModeScript for improved theming

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,14 @@
 'use client';
 
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
 
 const theme = extendTheme({
+  config,
   styles: {
     global: {
       body: {
